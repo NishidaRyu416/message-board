b29ede7c338cc0dac063884c186a1d73b2badf53
Ryu <nishidaryu416@gmail.com>
2016-07-24 15:09:47 +0000

    年齢機能実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,5 @@
 class MessagesController < ApplicationController
-  before_action :set_message, only: [:edit, :update,:destroy]
+  before_action :set_message, only: [:edit, :update, :destroy]
   
   def index
     @message = Message.new
@@ -39,7 +39,7 @@ class MessagesController < ApplicationController
 
   private
   def message_params
-    params.require(:message).permit(:name, :body)
+    params.require(:message).permit(:name, :body,:age)
   end
   ## ここまで
   
